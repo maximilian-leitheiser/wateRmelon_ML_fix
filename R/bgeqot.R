5bb1f4d3641b2520ad504e583b9986b1f4d3712e
maximilian-leitheiser <64211433+maximilian-leitheiser@users.noreply.github.com>
2024-12-20 13:16:30 +0100

    Adapt dasen for use with one sample

--- R/bgeqot.R
+++ R/bgeqot.R
@@ -112,11 +112,11 @@ dasen <-
 function(mns, uns, onetwo, fudge=100, ret2=FALSE, ...){
    mnsc <- dfsfit(mns,  onetwo, ...)  
    unsc <- dfsfit(uns,  onetwo, roco=NULL)
-   mnsc[onetwo=='I' ,] <- normalizeQuantiles(mnsc[onetwo=='I', ])
-   mnsc[onetwo=='II',] <- normalizeQuantiles(mnsc[onetwo=='II',])
+   mnsc[onetwo=='I' ,] <- normalizeQuantiles(mnsc[onetwo=='I', , drop = FALSE])
+   mnsc[onetwo=='II',] <- normalizeQuantiles(mnsc[onetwo=='II', , drop = FALSE])
 
-   unsc[onetwo=='I' ,] <- normalizeQuantiles(unsc[onetwo=='I', ])
-   unsc[onetwo=='II',] <- normalizeQuantiles(unsc[onetwo=='II',])
+   unsc[onetwo=='I' ,] <- normalizeQuantiles(unsc[onetwo=='I', , drop = FALSE])
+   unsc[onetwo=='II',] <- normalizeQuantiles(unsc[onetwo=='II', , drop = FALSE])
    beta <- mnsc/( mnsc + unsc + fudge )
    if (ret2) return (list(methylated=mnsc,unmethylated=unsc,beta=beta))
    beta
